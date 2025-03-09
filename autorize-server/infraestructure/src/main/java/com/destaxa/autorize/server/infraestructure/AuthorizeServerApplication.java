7b262729d049146c8fd43f99568c9876a6a67f9a
Alzemiro Iago de Moraes Thomaz <alzemiro.thomaz.ext@spms.min-saude.pt>
2025-03-09 15:57:45 -0300

    first commit

---
+++ autorize-server/infraestructure/src/main/java/com/destaxa/autorize/server/infraestructure/AuthorizeServerApplication.java
@@ -0,0 +1,12 @@
+package com.destaxa.autorize.server.infraestructure;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizeServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorizeServerApplication.class, args);
+    }
+}
